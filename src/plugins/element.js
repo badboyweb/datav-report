785e3cc8b96ed09523a06b47767a50d6f768d204
王强 <nashwq@163.com>
2022-01-15 17:23:20 +0800

    feat: 集成element-ui
    集成element-ui

---
+++ src/plugins/element.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import {
+  Card,
+  Col,
+  DatePicker,
+  Menu,
+  MenuItem,
+  Pagination,
+  RadioButton,
+  RadioGroup,
+  Row,
+  Table,
+  TableColumn
+} from 'element-ui'
+
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Pagination)
